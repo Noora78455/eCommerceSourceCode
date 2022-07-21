90813d8321b0ac6a2afc261383804da95aae1c08
Noora78455 <62265214+Noora78455@users.noreply.github.com>
2022-07-21 16:10:07 +0100

    Update functions.php

--- ecommerce/customer/functions/functions.php
+++ ecommerce/customer/functions/functions.php
@@ -1,6 +1,11 @@
 <?php 
 
-$con = mysqli_connect("localhost","root","","ecommerce");
+//$con = mysqli_connect("localhost","root","","ecommerce");
+
+$sslcert    = "ssl/DigiCertGlobalRootCA.crt.pem(1)";
+//$con = mysqli_connect("localhost","root","","ecommerce");
+$con = mysqli_init(); mysqli_ssl_set($con,NULL,NULL, "$sslcert", NULL, NULL); 
+mysqli_real_connect($conn, "mydbserveur.mysql.database.azure.com", "nourrad", "pass@1234", "db", 3306, MYSQLI_CLIENT_SSL);
 
 if (mysqli_connect_errno())
   {
